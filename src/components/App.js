4d28c629b36f8fe53527b86657c6f5ddf49b7780
abhikaushik <abhikaushik@paypal.com>
2021-05-17 21:47:31 +0530

    added function

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, toggle] = useState(false);
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() => toggle(!isClicked)}>
+        Add Paragraph
+      </button>
+      {isClicked ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
